8f498196e90a236fa9bec1121b2ccc7d28016607
florianbacle <florian.bacle@etu.univ-lyon1.fr>
2013-03-05 15:51:42 +0100

    bim

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(); 
                     
-            $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
+ 
 
         }
 
